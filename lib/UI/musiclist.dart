317aa813815d655af355910b43499ec4d7113db5
AngelaManoj <103014790+AngelaManoj@users.noreply.github.com>
2023-05-12 10:52:53 +0530

    changes

--- lib/UI/musiclist.dart
+++ lib/UI/musiclist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rhythmic/UI/Home.dart';
 import 'package:rhythmic/UI/music.dart';
 
 class MusicList extends StatefulWidget {
@@ -13,7 +14,21 @@ class _MusicListState extends State<MusicList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("song"),
-        backgroundColor: Color.fromRGBO(68, 44, 108, 1.0),),
+        backgroundColor: Color.fromRGBO(68, 44, 108, 1.0),
+        leading: IconButton(
+          color: Colors.transparent,
+
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => DestiniApp()));
+          },
+        ),
+      ),
       body: ListView.builder(
           itemCount: 30,
           itemBuilder: (BuildContext context, int index) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers
-import path_provider_foundation
+import flutter_tts
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
 }
